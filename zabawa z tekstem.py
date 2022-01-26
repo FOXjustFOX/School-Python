e8a772a2a1fbb7ae668522d9bfc63d0e46fedde3
FOXjustFOX <87518349+FOXjustFOX@users.noreply.github.com>
2022-01-26 18:37:20 +0100

    teraz
    tetar

--- zabawa z tekstem.py
+++ zabawa z tekstem.py
@@ -3,18 +3,20 @@ t = "To jest jaikiś tekst"
 n = len(t)
 licznik = 0
 
-#for i in range(n):
-#    if t[i] == "a":
-#        licznik += 1
+for i in range(n):
+    if t[i] == "a":
+        licznik += 1
     
 print(licznik)
 
-#print(
-#    t[0],"\n",
-#    t[1],"\n",
-#    t[3:6],"\n",
-#    t[7:],"\n",
-#    t[:7],"\n",
-#    t[ : :2],"\n",
-#    t[: :-1]
-#)
+"""
+print(
+    t[0],"\n",
+    t[1],"\n",
+    t[3:6],"\n",
+    t[7:],"\n",
+    t[:7],"\n",
+    t[ : :2],"\n",
+    t[: :-1]
+)
+"""
