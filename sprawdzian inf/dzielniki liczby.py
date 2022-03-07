1a78eddb7b81c252ba17dc1a9ba33e7f6f8adf00
FOXjustFOX <87518349+FOXjustFOX@users.noreply.github.com>
2022-03-07 15:37:57 +0100

    some comments

--- sprawdzian inf/dzielniki liczby.py
+++ sprawdzian inf/dzielniki liczby.py
@@ -1,7 +1,9 @@
 a = int(input("podaj liczbę do wyznaczania dzialników: ")) #wprować zmienną której dzielników będziemy szukać
 wyniki = [] # tworzymy listę, w którą włożymy dzielniki liczby a
 
-if a > 0:   #sprawdza czy liczba jest liczbą naturalną
+
+
+if a > 0:   #sprawdza czy liczba jest liczbą naturalną (nie jest ona obowiązkowa! można ją usunąć)
     for i in range(1, int(a/2) + 1): #iterujemy od jedynki, do połowy liczby x - dzielenie przez wiecej niż połowe nie da całkowitego wyniku
         if a % i == 0:       #jeżeli liczba i  jest dzielnikiem liczby x
             wyniki.append(i) #to dodaj ją do listy wyników 
