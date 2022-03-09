82b968199a896cd1fb695649fe1a5cfa6686bcf8
FOXjustFOX <87518349+FOXjustFOX@users.noreply.github.com>
2022-03-09 15:34:27 +0100

    working robot

--- robot.py
+++ robot.py
@@ -6,13 +6,13 @@ l = a.split(" ")
 print(l)
 
 for i in l:
-    if i[1] == "G":
-        y+= int(i[0])
-    elif i[1] == "D":
-        y -= int(i[0])
-    elif i[1] == "L":
-        x -= int(i[0])
-    elif i[1] == "R":
-        x += int(i[0])
+    if i[-1] == "G":
+        y+= int(i[:-1])
+    elif i[-1] == "D":
+        y -= int(i[:-1])
+    elif i[-1] == "L":
+        x -= int(i[:-1])
+    elif i[-1] == "R":
+        x += int(i[:-1])
 
 print(x, y)
