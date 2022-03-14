c61d13898de602ea46909cc9f85d2c9ed712683c
FOXjustFOX <87518349+FOXjustFOX@users.noreply.github.com>
2022-03-14 07:53:59 +0100

    update algorytm euklidesa.py

--- algorytm euklidesa.py
+++ algorytm euklidesa.py
@@ -4,7 +4,7 @@ b = int(input("podaj 2 liczbę: "))
 while a != b: #dopóki a nie jest równe b to iterujemy
     if a < b:   #jeśli a jest większe od b to zamień je miejscami
         a, b = b, a 
-    a -= b #pod a wstawiamy liczbę a - b
+    a, b = b, a%b #pod a wstawiamy liczbę a - b
 
 print(int(a))
 
