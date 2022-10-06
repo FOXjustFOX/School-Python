199b40084278078cdb14947d2689282d7b316dff
FOXjustFOX <87518349+FOXjustFOX@users.noreply.github.com>
2022-10-06 14:35:15 +0200

    Create liczenie_liczb.py

---
+++ liczenie_liczb.py
@@ -0,0 +1,18 @@
+
+a = int(input())
+
+s_alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+#b_alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+z = {
+
+}
+
+for i in range(a):
+    b = input()
+    for j in s_alphabet:
+        z[j] += b.count(j) if z[j] != 0 else 
+
+#for i in range(len(z)):
+print(z)
