0275f7eb5a25410a8341e5ae08fc68ba5a3c913c
FOXjustFOX <iglis.com20@gmail.com>
2022-10-11 07:49:08 +0000

    yes

--- liczby_super_b_pierwsze.py
+++ liczby_super_b_pierwsze.py
@@ -83,53 +83,29 @@ def other(a, n):
 
 
 
-
-
-
-
-
-
-
 def SieveOfAtkin(d, limit):
     
     t0 = time.time()
     
     tab = []
 
-    # 2 and 3 are known
-    # to be prime
+    
     if limit > 2:
-        print(2, end=" ")
+        tab.append(2)
     if limit > 3:
-        print(3, end=" ")
+        tab.append(3)
  
-    # Initialise the sieve
-    # array with False values
     sieve = [False] * (limit + 1)
     for i in range(0, limit + 1):
         sieve[i] = False
  
-    '''Mark sieve[n] is True if
-    one of the following is True:
-    a) n = (4*x*x)+(y*y) has odd
-    number of solutions, i.e.,
-    there exist odd number of
-    distinct pairs (x, y) that
-    satisfy the equation and
-    n % 12 = 1 or n % 12 = 5.
-    b) n = (3*x*x)+(y*y) has
-    odd number of solutions
-    and n % 12 = 7
-    c) n = (3*x*x)-(y*y) has
-    odd number of solutions,
-    x > y and n % 12 = 11 '''
+    
     x = 1
     while x * x <= limit:
         y = 1
         while y * y <= limit:
  
-            # Main part of
-            # Sieve of Atkin
+        
             n = (4 * x * x) + (y * y)
             if (n <= limit and (n % 12 == 1 or
                                 n % 12 == 5)):
@@ -146,8 +122,6 @@ def SieveOfAtkin(d, limit):
             y += 1
         x += 1
  
-    # Mark all multiples of
-    # squares as non-prime
     r = 5
     while r * r <= limit:
         if sieve[r]:
@@ -156,11 +130,9 @@ def SieveOfAtkin(d, limit):
  
         r += 1
  
-        # Print primes
-    # using sieve[]
+        
     for a in range(5, limit+1):
         if sieve[a]:
-            #print(a, end=" ")
             tab.append(a)
     
     
