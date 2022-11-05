5dbe94fce1924d7807483699d0ed316cd8dc7416
FOXjustFOX <iglis.com20@gmail.com>
2022-11-05 14:46:19 +0100

    modified files for my classmates (1etap_konkurs)

--- konkurs 3lo etap 1/python/jewelery.py
+++ konkurs 3lo etap 1/python/jewelery.py
@@ -5,22 +5,22 @@ for i in range(n):
     jewels.append(input())
 
 jewels.sort(key= lambda x: (len(x) + 1) ** 15 + ord(x[0]))
-for _ in range(5):
-    for i in range(len(jewels)):
-        try:
-            jewel = jewels[i]
-            nexjewel = jewels[i+1]
-        except IndexError:
-            pass
-        else:
-            if len(jewel) == len(nexjewel):
-                j = 0
-                while jewel[j] == nexjewel[j]:
-                    j+=1
 
-                else:
-                    if ord(jewel[j]) > ord(nexjewel[j]):
-                        jewels[i], jewels[i+1] = nexjewel, jewel
+for i in range(len(jewels)):
+    try:
+        jewel = jewels[i]
+        nexjewel = jewels[i+1]
+    except IndexError:
+        pass
+    else:
+        if len(jewel) == len(nexjewel):
+            j = 0
+            while jewel[j] == nexjewel[j]:
+                j+=1
+
+            else:
+                if ord(jewel[j]) > ord(nexjewel[j]):
+                    jewels[i], jewels[i+1] = nexjewel, jewel
 
 
 for jewel in jewels:
